52bbdfb70f55499d318c089c773a19ecdb988b3c
Jiwon Lee <drmgil44@gmail.com>
2020-09-29 14:25:23 +0900

    rest api 'GET' from Django

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function LandingPage() {
-    return (
-        <div>
-            landing page
-        </div>
-    )
+  // componentDidMount, componentDidUpdate와 같은 방식으로
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8000/test/board/')
+      .then((response) => console.log(response.data));
+  }, []);
+
+  return <div>LandingPage</div>;
 }
 
-export default LandingPage
+export default LandingPage;
